b412070cb64835f6c7dad8d7b4a8b21b9840a961
woe <86549645+d29l@users.noreply.github.com>
2025-02-19 00:56:25 +0100

    Added basic functionality for signing in

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <nav class="bg-base pb-1 h-[8vh]">
-      <div class="flex items-center h-full shadow-md shadow-crust">
-        <div class="pl-4">
-          <h1 class="text-lavender font-extrabold text-2xl">Job platform</h1>
+    <nav className="h-[8vh] bg-base pb-1">
+      <div className="flex h-full items-center justify-between shadow-md shadow-crust">
+        <div className="flex items-center space-x-6 pl-4">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-extrabold text-lavender">
+              Job platform
+            </h1>
+          </div>
+
+          <div className="flex space-x-4"></div>
         </div>
+
+        <div className="mr-4 flex size-10 overflow-hidden rounded-full bg-surface0"></div>
       </div>
     </nav>
-  )
+  );
 }
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,43 +1,94 @@
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const navigate = useNavigate()
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "https://localhost:7077/api/users/loginUser",
+        {
+          email,
+          password,
+        },
+      );
+
+      if (response.status == 200) {
+        navigate("/");
+        console.log(response.data);
+        console.log("Successfully signed in");
+        localStorage.setItem("token", response.data.token);
+      } else {
+        console.log(response.data.message || "Sign in failed");
+      }
+    } catch (err) {
+      console.log(
+        err.response?.data?.message || "An error occurred during signing in",
+      );
+    }
+  };
+
+  const navigate = useNavigate();
   const handleRedirect = () => {
     navigate(`/signup`);
   };
 
   return (
-    <div class=" min-h-[92vh] flex justify-center items-center">
-      <div class="pt-6 bg-base w-[24rem] rounded-lg py-10 shadow-md shadow-crust">
-        <div class="flex  justify-center">
-          <h1 class="text-lavender font-extrabold text-2xl">Sign in</h1>
+    <div class="flex min-h-[92vh] items-center justify-center">
+      <div class="w-[24rem] rounded-lg bg-base py-10 pt-6 shadow-md shadow-crust">
+        <div class="flex justify-center">
+          <h1 class="text-2xl font-extrabold text-lavender">Sign in</h1>
         </div>
-        <div class="flex flex-col items-start pt-8 px-8">
-          <form class="flex flex-col w-full">
-            <label class="mb-2 text-text font-bold">Email</label>
-            <input placeholder="example@mail.com" class="pl-2 placeholder-surface2 text-subtext0 mb-2 rounded-lg bg-mantle h-8 focus:border-2 focus:border-lavender focus:outline-none" />
-            <label class="mb-2 text-text font-bold">Password</label>
-            <input type="password" class="pl-2 text-subtext0 mb-2 rounded-lg bg-mantle h-8 focus:border-2 focus:border-lavender focus:outline-none" />
+        <div class="flex flex-col items-start px-8 pt-8">
+          <form class="flex w-full flex-col" onSubmit={handleLogin}>
+            <label class="mb-2 font-bold text-text">Email</label>
+            <input
+              placeholder="example@mail.com"
+              class="mb-2 h-8 rounded-lg bg-mantle pl-2 text-subtext0 placeholder-surface2 focus:border-2 focus:border-lavender focus:outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label class="mb-2 font-bold text-text">Password</label>
+            <input
+              type="password"
+              class="mb-2 h-8 rounded-lg bg-mantle pl-2 text-subtext0 focus:border-2 focus:border-lavender focus:outline-none"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-            <div class="flex items-center mt-2">
-              <input type="checkbox" class="appearance-none w-4 h-4 border-2 rounded border-subtext1 checked:bg-lavender cursor-pointer transition-colors duration-200 focus:outline-none" />
-              <label class="ml-2 text-subtext1 cursor-pointer select-none">Keep me signed in</label>
-            </div>
+            {/* <div class="mt-2 flex items-center">
+              <input
+                type="checkbox"
+                class="h-4 w-4 cursor-pointer appearance-none rounded border-2 border-subtext1 transition-colors duration-200 checked:bg-lavender focus:outline-none"
+              />
+              <label class="ml-2 cursor-pointer select-none text-subtext1">
+                Keep me signed in
+              </label>
+            </div> */}
 
-            <div class="flex justify-center mt-6">
-              <button class="p-1 bg-lavender rounded-lg w-1/2 text-mantle font-bold">Sign in</button>
+            <div class="mt-6 flex justify-center">
+              <button class="w-1/2 rounded-lg bg-lavender p-1 font-bold text-mantle">
+                Sign in
+              </button>
             </div>
 
             <div class="mt-6 flex flex-row justify-center">
               <p class="text-subtext1">Don't have an account yet?</p>
-              <a class="text-lavender ml-2 hover:underline cursor-pointer" onClick={handleRedirect}>Sign up</a>
+              <a
+                class="ml-2 cursor-pointer text-lavender hover:underline"
+                onClick={handleRedirect}
+              >
+                Sign up
+              </a>
             </div>
-
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
