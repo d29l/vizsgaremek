b193bc218ab5503ab640ef403ff23694142935e2
AblonczyAttila <ablonczya@kkszki.hu>
2025-02-20 10:27:13 +0100

    Usercs created

---
+++ AdminPanel/AdminPanel/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanel
+{
+    public partial class User
+    {
+        public int UserID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
